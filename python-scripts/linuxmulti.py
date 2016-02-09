7c28ac3f73ff9479b6a424afd9557bfbbfe33796
tsullens <tsullens@mix.wvu.edu>
2016-02-09 12:18:48 -0500

    Update linuxmulti.py

--- python-scripts/linuxmulti.py
+++ python-scripts/linuxmulti.py
@@ -4,21 +4,19 @@ import sys, getopt, subprocess, os
 from multiprocessing import Process, Pool
 from subprocess import Popen, PIPE
 
+pwd = ""
+
 def printHelp(err = False):
   if err:
     print err + "\n"
-  print """Usage:
-           -h | --help -> print help
-           -t | --threads {num_threads} -> where {num_threads is the upper limit of parallel processes
-           -i | --input {inputfile} -> where {inputfile} is list of servers
-           \"command[;command]\""""
+  print "\tUsage: -h | --help -> print help\n\t-t | --threads {num_threads} -> where {num_threads is the upper limit of parallel processes\n\t-s | --sudo -> use sudo to execute each command\n\t-i | --input {inputfile} -> where {inputfile} is list of servers\n\t\"command[;command]\"\n\t Ex. ./linuxmulti.py -t 8 -i srv-list \"uptime;date\""
 
 
 def run(srv, commands):
-  print "HOST: %s" % srv
-  print commands
-  print "ID: %d" % os.getpid()
-  ssh = subprocess.Popen(["ssh", "-q", srv, "%s" % commands], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  print "HOST: " + srv
+#  print commands
+#  print "ID: %d" % os.getpid()
+  ssh = subprocess.Popen(["sshpass", "-p", pwd, "ssh", "-q", "tyler.sullens@%s" % srv, "%s" % commands], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   result,err = ssh.communicate()
   if result:
     print result
@@ -26,40 +24,30 @@ def run(srv, commands):
     print err
   print ""
 
+
+def sudo_format(comms):
+  newstr = ""
+  for comm in comms.split(";"):
+    newstr += "echo \'" + pwd + "\' | sudo -S " + comm.strip() + ";"
+  return newstr
+
 def main(argv):
   threads = 4
   try:
-    opts, args = getopt.getopt(argv,"hi:t:",["help", "input", "threads"])
+    opts, args = getopt.getopt(argv,"hsi:t:",["help", "sudo", "input", "threads"])
   except getopt.GetoptError:
     printHelp()
     sys.exit()
-  if opts: 
+  comms = args[0]
+  if opts:
     for opt, arg in opts:
       if opt in ("-h", "--help"):
         printHelp()
         sys.exit()
       if opt in ("-t", "--threads"):
         threads = int(arg)
+      if opt in ("-s", "--sudo"):
+        comms = sudo_format(comms)
+        print comms
       if opt in ("-i", "--input"):
         if len(args):
-          try:
-            f = open(arg, 'r')
-          except:
-            printHelp("File read error")
-            sys.exit()
-          srvs = []
-          try:
-            pool = Pool(processes=threads)
-            results = [pool.apply_async(run, args=(srv,  args[0])).get(30) for srv in f]
-          except:
-            print"Failure: thread block, exiting\n"
-            sys.exit()
-        else:
-          printHelp("No command list found")
-      else: 
-        printHelp()
-  else:
-    printHelp()
-
-if __name__ == "__main__":
-  main(sys.argv[1:])
